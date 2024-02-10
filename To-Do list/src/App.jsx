6eca38ee55ebfb0c65f932150bea32b7add8f79f
Vitor Valerio Hoffmann <vitorvaleriohoffmann@gmail.com>
2024-02-09 23:56:30 -0300

    To-Do List

---
+++ To-Do list/src/App.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+  const [checkedTasks, setCheckedTasks] = useState([]);
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, newTask]);
+      setCheckedTasks([...checkedTasks, false]); // Inicializa o estado de cada checkbox como falso
+      setNewTask('');
+    }
+  };
+
+  const removeTask = (index) => {
+    const updatedTasks = [...tasks];
+    const updatedCheckedTasks = [...checkedTasks];
+
+    updatedTasks.splice(index, 1);
+    updatedCheckedTasks.splice(index, 1);
+
+    setTasks(updatedTasks);
+    setCheckedTasks(updatedCheckedTasks);
+  };
+
+  const handleCheckboxChange = (index) => {
+    const updatedCheckedTasks = [...checkedTasks];
+    updatedCheckedTasks[index] = !updatedCheckedTasks[index];
+    setCheckedTasks(updatedCheckedTasks);
+  };
+
+  return (
+    <div id='div-geral'>
+      <div id='div-title'>
+        <h1 id='text'>To-Do List</h1>
+        <div id='input'>
+          <input
+            id='in'
+            placeholder='New task'
+            className='textin'
+            type="text"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+          />
+          <button onClick={addTask}>Add</button>
+        </div>
+      </div>
+
+      <div id='div-tasks'>
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index} className={`list ${checkedTasks[index] ? 'Checked' : ''}`}>
+              <div id='buttons2'>
+                <button onClick={() => handleCheckboxChange(index)}>Done</button>
+                <button className='button' onClick={() => removeTask(index)}>Remove</button>
+              </div>
+              {index + 1} - {task}
+              
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
